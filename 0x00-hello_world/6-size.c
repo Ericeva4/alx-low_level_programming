6ecf7e90dadaa9eb2ba7addef4706eb5e5479336
Ericeva4 <ericeva44@gmail.com>
2023-08-10 19:12:32 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: always 0 (success)
  */
 int main(void)
-{	
+{
 	printf("Size of a char: %d byte(s)", sizeof(char));
 	printf("Size of an int: %d byte(s)", sizeof(int));
 	printf("Size of a long int: %d byte(s)", sizeof(long int));
