cf2080fbcba54369a63d59988e0dc270e2fa6561
Ericeva4 <ericeva44@gmail.com>
2023-08-17 16:24:32 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function checks for a digit(0 to 9).
+ * Return: Always return 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
