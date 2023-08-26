270fbc8c9b65228d216c713a2128ede36cf47aab
Ericeva4 <ericeva44@gmail.com>
2023-08-25 18:03:10 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,11 +14,11 @@ void print_number(int n)
 	if (n  < 0)
 	{
 		_putchar('-');
-		n1 = n;
+		n1 = -n;
 	}
 	if (n1 / 10 != 0)
 	{
-		print_numbers(n1 / 10);
+		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
